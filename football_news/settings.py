a7f66fcd6f0e270a552bc4e6ae125c3d6c6cfd58
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2025-09-10 14:13:56 +0700

    solve bug

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rafsanjani41-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://rafsanjani41-footballnews.pbp.cs.ui.ac.id/"
+    "https://rafsanjani41-footballnews.pbp.cs.ui.ac.id",
 ]
 
 # Application definition
